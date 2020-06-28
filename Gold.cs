d1a73e16699e600e117d4dbca043abc707c81aac
nikolova18 <67506297+nikolova18@users.noreply.github.com>
2020-06-28 22:32:58 +0300

    Add files via upload

---
+++ Gold.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketStore
+{
+    public class Gold
+    {
+        public double Turnover { get; set; }
+        public double DiscountRate { get; set; } = 0.02;
+        public double PurchaseValue { get; set; }
+
+        public Gold(double turnover, double purchaseValue)
+        {
+            this.Turnover = turnover;
+            this.PurchaseValue = purchaseValue;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Gold:");
+            sb.AppendLine($"a. Mock data: turnover ${this.Turnover}, purchase value ${this.PurchaseValue}");
+            sb.AppendLine($"b. Ouput:");
+            sb.AppendLine($"o   Purchase value: ${this.PurchaseValue:F2}" +
+                $"\no   Discount rate: {this.DiscountRate * 100:F1}%\no   Discount: ${(this.PurchaseValue * this.DiscountRate):F2}" +
+                $"\no   Total: ${this.PurchaseValue - (this.PurchaseValue * this.DiscountRate):F2}");
+            return sb.ToString().TrimEnd();
+        }
+        
+
+
+    }
+}
---
+++ Silver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarketStore
+{
+    public class Silver
+    {
+        public double Turnover { get; set; }
+        public double DiscountRate { get; set; } =0.02;
+        public double PurchaseValue { get; set; }
+
+        public Silver(double turnover,double purchaseValue)
+        {
+            this.Turnover = turnover;
+            this.PurchaseValue = purchaseValue;
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Silver:");
+            sb.AppendLine($"a. Mock data: turnover ${this.Turnover}, purchase value ${this.PurchaseValue}");
+            sb.AppendLine($"b. Ouput:");
+            sb.AppendLine($"o   Purchase value: ${this.PurchaseValue:F2}" +
+                $"\no   Discount rate: {this.DiscountRate*100:F1}%\no   Discount: ${(this.PurchaseValue * this.DiscountRate):F2}" +
+                $"\no   Total: ${this.PurchaseValue - (this.PurchaseValue * this.DiscountRate)}");
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
